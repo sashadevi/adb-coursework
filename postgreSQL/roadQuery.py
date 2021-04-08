937fac3b83ed1ca4f9efb55cf633c9592fe9ed96
Your Name <sashadevi@me.com>
2021-04-08 18:32:11 +0100

    add queries

---
+++ postgreSQL/roadQuery.py
@@ -0,0 +1,32 @@
+import psycopg2
+
+#establishing the connection
+conn = psycopg2.connect(
+   database="postgres", user='hai', password='12345678', host='127.0.0.1', port= '5432'
+)
+conn.autocommit = True
+
+#Creating a cursor object using the cursor() method
+cursor = conn.cursor()
+
+city = input("Please enter location where you would like to search (type C for Cambridge or WM for West Midlands): ")
+
+if city == "WM":
+    road = input("Please enter a road: ")
+    findCrimeType = ''' SELECT DISTINCT Crime_type FROM west_midlands_data WHERE Location = 'On or near ''' + road + '''' ;'''
+    cursor.execute(findCrimeType)
+    list_tables = cursor.fetchall()
+    print(list_tables)
+elif city=="C":
+    road = input("Please enter a road: ")
+    findCrimeType = ''' SELECT DISTINCT Crime_type FROM cambridge_data WHERE Location = 'On or near ''' + road + '''' ;'''
+    cursor.execute(findCrimeType)
+    list_tables = cursor.fetchall()
+    print(list_tables)
+else:
+    print("Sorry, you did not enter a valid location")
+
+
+
+
+conn.close()
