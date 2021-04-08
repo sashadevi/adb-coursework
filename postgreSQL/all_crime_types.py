5e12010406390c82a42104e495fffdcbc714901e
Pooja <poojarenganathan@gmail.com>
2021-04-08 23:29:19 +0100

    minor fixes

--- postgreSQL/all_crime_types.py
+++ postgreSQL/all_crime_types.py
@@ -9,22 +9,18 @@ conn.autocommit = True
 #creating a cursor object using the cursor() method
 cursor = conn.cursor()
 
-crime_types = input("To find out all crime types in the West Midlands and Cambridge enter 'all crime types': ");
-if crime_types == "all crime types":
-	
-	#select the different crime types from west midlands table
-	midlands_crime_types = '''SELECT DISTINCT Crime_type FROM west_midlands_data; '''
+#select the different crime types from west midlands table
+midlands_crime_types = '''SELECT DISTINCT Crime_type FROM west_midlands_data; '''
 
-	#select the different crime types from cambridge table
-	cambridge_crime_types = '''SELECT DISTINCT Crime_type FROM cambridge_data;'''
+#select the different crime types from cambridge table
+cambridge_crime_types = '''SELECT DISTINCT Crime_type FROM cambridge_data;'''
 
-	#execute query
-	cursor.execute(midlands_crime_types, cambridge_crime_types)
+#execute query
+cursor.execute(midlands_crime_types, cambridge_crime_types)
 
-	list_crime_types = cursor.fetchall()
-	print(list_crime_types)
-else : 
-	print("command not found")
+list_crime_types = cursor.fetchall()
+print(list_crime_types)
+
 
 
 
