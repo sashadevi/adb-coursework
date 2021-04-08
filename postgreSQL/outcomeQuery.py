bf7d13f77381375733b20e9388b78a586ec37846
Your Name <sashadevi@me.com>
2021-04-08 19:08:11 +0100

    create new query

--- postgreSQL/outcomeQuery.py
+++ postgreSQL/outcomeQuery.py
@@ -9,4 +9,13 @@ conn.autocommit = True
 #Creating a cursor object using the cursor() method
 cursor = conn.cursor()
 
+crimeType=input("Please enter a crime type: ")
+
+select_outcome_category= "SELECT MAX(Last_outcome_category) FROM west_midlands_data WHERE Crime_type = '"+ crimeType + "';"
+select_outcome_category1= "SELECT MAX(Last_outcome_category) FROM cambridge_data WHERE Crime_type = '"+ crimeType + "';"
+cursor.execute(select_outcome_category, select_outcome_category1)
+
+list_tables = cursor.fetchall()
+print(list_tables)
+
 conn.close()
