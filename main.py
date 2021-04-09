2651392414ddea97e222e13f5155032d28e9c00b
Your Name <sashadevi@me.com>
2021-04-09 17:22:39 +0100

    add expressions for three columns

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from postgreSQL.twoColumns import *
+from postgreSQL.threeColumns import *
 
 import psycopg2
 
@@ -23,5 +24,20 @@ crimesByLSOA(lsoa_name)
 outcome = input("Please enter an outcome category. Press 'enter' if you would like to leave this blank: ")
 crimesByOutcome(outcome)
 
+location = input("Please enter a location. Press 'enter' if you would like to leave this blank: ")
+crime_type = input("Please enter crime type. Press 'enter' if you would like to leave this blank: ")
+crimesByCrimeTypeLocation(crime_type, location)
+
+crime_type = input("Please enter crime type. Press 'enter' if you would like to leave this blank: ")
+outcome = input("Please enter an outcome category. Press 'enter' if you would like to leave this blank: ")
+crimesByTypeOutcome(crime_type, outcome)
+
+location = input("Please enter a location. Press 'enter' if you would like to leave this blank: ")
+outcome = input("Please enter an outcome category. Press 'enter' if you would like to leave this blank: ")
+crimesByLocationOutcome(location, outcome)
+
+outcome = input("Please enter an outcome category. Press 'enter' if you would like to leave this blank: ")
+lsoa = input("Please enter a LSOA name. Press 'enter' if you would like to leave this blank: ")
+crimesByLSOAOutcome(lsoa, outcome)
 
 conn.close()
--- postgreSQL/threeColumns.py
+++ postgreSQL/threeColumns.py
@@ -9,40 +9,68 @@ conn.autocommit = True
 #creating a cursor object using the cursor() method
 cursor = conn.cursor()
 
-def crimesByCrimeTypeLocation():
-	crimesByCrimeTypeLocation = '''SELECT Falls_within, Crime_type, Location, COUNT(Crime_type) FROM police_data GROUP BY Falls_within, Crime_type, Location ORDER BY Falls_within, Crime_type, COUNT(Crime_type) DESC; '''
+def crimesByCrimeTypeLocation(crime_type, location):
+	if crime_type == "" and location == "":
+		crimesByCrimeTypeLocation = '''SELECT Falls_within, Crime_type, Location, COUNT(Crime_type) FROM police_data GROUP BY Falls_within, Crime_type, Location ORDER BY Falls_within, Crime_type, COUNT(Crime_type) DESC; '''
+	elif crime_type == "" and location != "":
+		crimesByCrimeTypeLocation = "SELECT Falls_within, Crime_type, Location, COUNT(Crime_type) FROM police_data WHERE Location = 'On or near " + location + "' GROUP BY Falls_within, Crime_type, Location ORDER BY Falls_within, Crime_type, COUNT(Crime_type) DESC;"
+	elif crime_type != "" and location == "":
+		crimesByCrimeTypeLocation = "SELECT Falls_within, Crime_type, Location, COUNT(Crime_type) FROM police_data WHERE Crime_type = '" + crime_type + "' GROUP BY Falls_within, Crime_type, Location ORDER BY Falls_within, Crime_type, COUNT(Crime_type) DESC;"
+	else:
+		crimesByCrimeTypeLocation = "SELECT Falls_within, Crime_type, Location, COUNT(Crime_type) FROM police_data WHERE Location = 'On or near " + location + "' AND Crime_type = '" + crime_type + "' GROUP BY Falls_within, Crime_type, Location ORDER BY Falls_within, Crime_type, COUNT(Crime_type) DESC;"
 	cursor.execute(crimesByCrimeTypeLocation)
 	list_tables = cursor.fetchall()
 	for row in list_tables:
 		print(f"{row[0]} {row[1]} {row[2]} {row[3]}")
         
 
-def crimesByTypeOutcome(): 
-	crimesByTypeOutcome = '''SELECT Falls_within, Crime_type, Last_outcome_category, COUNT(Crime_type) FROM police_data GROUP BY Falls_within, Crime_type, Last_outcome_category ORDER BY Falls_within, Crime_type, COUNT(Crime_type) DESC; '''
+def crimesByTypeOutcome(crime_type, outcome):
+	if crime_type == "" and outcome == "":
+		crimesByTypeOutcome = '''SELECT Falls_within, Crime_type, Last_outcome_category, COUNT(Crime_type) FROM police_data GROUP BY Falls_within, Crime_type, Last_outcome_category ORDER BY Falls_within, Crime_type, COUNT(Crime_type) DESC; '''
+	elif crime_type == "" and outcome != "":
+		crimesByTypeOutcome = "SELECT Falls_within, Crime_type, Last_outcome_category, COUNT(Crime_type) FROM police_data WHERE Last_outcome_category = '" + outcome + "' GROUP BY Falls_within, Crime_type, Last_outcome_category ORDER BY Falls_within, Crime_type, COUNT(Crime_type) DESC;"
+	elif crime_type != "" and outcome == "":
+		crimesByTypeOutcome = "SELECT Falls_within, Crime_type, Last_outcome_category, COUNT(Crime_type) FROM police_data WHERE Crime_type = '" + crime_type + "' GROUP BY Falls_within, Crime_type, Last_outcome_category ORDER BY Falls_within, Crime_type, COUNT(Crime_type) DESC;"
+	else:
+		crimesByTypeOutcome = "SELECT Falls_within, Crime_type, Last_outcome_category, COUNT(Crime_type) FROM police_data WHERE Crime_type = '" + crime_type + "' AND Last_outcome_category = '" + outcome + "' GROUP BY Falls_within, Crime_type, Last_outcome_category ORDER BY Falls_within, Crime_type, COUNT(Crime_type) DESC;"
 	cursor.execute(crimesByTypeOutcome)
 	list_tables = cursor.fetchall()
 	for row in list_tables:
 		print(f"{row[0]} {row[1]} {row[2]} {row[3]}")
 
 
-def crimesByLocationOutcome() :
-	crimesByLocationOutcome = '''SELECT Falls_within, Location, Last_outcome_category, COUNT(Crime_type) FROM police_data GROUP BY 	Falls_within, Location, Last_outcome_category ORDER BY Falls_within, Location, COUNT(Crime_type) DESC; '''
+def crimesByLocationOutcome(location, outcome):
+	if location == "" and outcome == "":
+		crimesByLocationOutcome = '''SELECT Falls_within, Location, Last_outcome_category, COUNT(Crime_type) FROM police_data GROUP BY 	Falls_within, Location, Last_outcome_category ORDER BY Falls_within, Location, COUNT(Crime_type) DESC; '''
+	elif location == "" and outcome != "":
+		crimesByLocationOutcome = "SELECT Falls_within, Location, Last_outcome_category, COUNT(Crime_type) FROM police_data WHERE Last_outcome_category = '"+ outcome +"' GROUP BY Falls_within, Location, Last_outcome_category ORDER BY Falls_within, Location, COUNT(Crime_type) DESC; "
+	elif location != "" and outcome == "":
+		crimesByLocationOutcome = "SELECT Falls_within, Location, Last_outcome_category, COUNT(Crime_type) FROM police_data WHERE Location = 'On or near " + location + "' GROUP BY Falls_within, Location, Last_outcome_category ORDER BY Falls_within, Location, COUNT(Crime_type) DESC; "
+	else:
+		crimesByLocationOutcome = "SELECT Falls_within, Location, Last_outcome_category, COUNT(Crime_type) FROM police_data WHERE Last_outcome_category = '"+ outcome +"' AND Location = 'On or near " + location + "' GROUP BY Falls_within, Location, Last_outcome_category ORDER BY Falls_within, Location, COUNT(Crime_type) DESC; "
 	cursor.execute(crimesByLocationOutcome)
 	list_tables = cursor.fetchall()
 	for row in list_tables:
 		print(f"{row[0]} {row[1]} {row[2]} {row[3]}")
        
         
-def crimesByLSOAOutcome():
-	crimesByLSOA = '''SELECT Falls_within, LSOA_name, Last_outcome_category, COUNT(Crime_type) FROM police_data GROUP BY Falls_within, LSOA_name, Last_outcome_category ORDER BY Falls_within, LSOA_name, COUNT(Crime_type) DESC; '''
+def crimesByLSOAOutcome(lsoa, outcome):
+	if lsoa == "" and outcome == "":
+		crimesByLSOA = '''SELECT Falls_within, LSOA_name, Last_outcome_category, COUNT(Crime_type) FROM police_data GROUP BY Falls_within, LSOA_name, Last_outcome_category ORDER BY Falls_within, LSOA_name, COUNT(Crime_type) DESC; '''
+	elif lsoa == "" and outcome != "":
+		crimesByLSOA = "SELECT Falls_within, LSOA_name, Last_outcome_category, COUNT(Crime_type) FROM police_data WHERE Last_outcome_category = '" + outcome + "' GROUP BY Falls_within, LSOA_name, Last_outcome_category ORDER BY Falls_within, LSOA_name, COUNT(Crime_type) DESC; "
+	elif lsoa != "" and outcome == "":
+		crimesByLSOA = "SELECT Falls_within, LSOA_name, Last_outcome_category, COUNT(Crime_type) FROM police_data WHERE LSOA_name = '" + lsoa + "' GROUP BY Falls_within, LSOA_name, Last_outcome_category ORDER BY Falls_within, LSOA_name, COUNT(Crime_type) DESC; "
+	else:
+		crimesByLSOA = "SELECT Falls_within, LSOA_name, Last_outcome_category, COUNT(Crime_type) FROM police_data WHERE Last_outcome_category = '" + outcome + "' AND LSOA_name = '" + lsoa + "' GROUP BY Falls_within, LSOA_name, Last_outcome_category ORDER BY Falls_within, LSOA_name, COUNT(Crime_type) DESC; "
 	cursor.execute(crimesByLSOA)
 	list_tables = cursor.fetchall()
 	for row in list_tables:
 		print(f"{row[0]} {row[1]} {row[2]} {row[3]}")
 
 
-crimesByCrimeTypeLocation()
-crimesByTypeOutcome()
-crimesByLocationOutcome()
-crimesByLSOAOutcome()
+# crimesByCrimeTypeLocation()
+# crimesByTypeOutcome()
+# crimesByLocationOutcome()
+# crimesByLSOAOutcome()
 
