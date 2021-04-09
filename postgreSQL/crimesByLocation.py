73af0dd2b757ae59ea8a55381b3ba8e590b4db8e
Your Name <sashadevi@me.com>
2021-04-09 12:09:30 +0100

    add new queries etc

---
+++ postgreSQL/crimesByLocation.py
@@ -0,0 +1,20 @@
+import psycopg2
+
+#establishing the connection
+conn = psycopg2.connect(
+   database="postgres", user='hai', password='12345678', host='127.0.0.1', port= '5432'
+)
+conn.autocommit = True
+
+#Creating a cursor object using the cursor() method
+cursor = conn.cursor()
+
+query = '''SELECT Falls_within, Location, COUNT(Crime_type) FROM police_data GROUP BY Falls_within, Location ORDER BY Falls_within, COUNT(Crime_type) DESC; '''
+cursor.execute(query)
+
+list_tables = cursor.fetchall()
+for row in list_tables:
+    print(f"{row[0]} {row[1]} {row[2]}")
+
+
+conn.close()
