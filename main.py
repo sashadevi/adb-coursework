c58f8f5842b05091127989a21097789aea05a8b3
Pooja <poojarenganathan@gmail.com>
2021-04-09 20:07:30 +0100

    altered user input statements

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ cursor = conn.cursor()
 
 keyWord = ''
 
-print("Welcome, if you would like to quit this program: ")
+print("Welcome, this application compares police data for the West Midlands and Cambridge for January 2021")
 while keyWord != 'q':
 
     keyWord = input("Please enter an option. If you would like to see all options press 'h'. If you want to quit, press 'q': ")
@@ -27,39 +27,39 @@ while keyWord != 'q':
         allCrimeTypes()
 
     elif keyWord == "2":
-        crime_type = input("Please enter the crime type. Press 'enter' if you would like to leave this blank: ")
+        crime_type = input("Please enter the crime type. Press 'enter' if you would like to view all crimes by crime type: ")
         crimesByCrimeType(crime_type)
 
     elif keyWord == "3":
-        location = input("Please enter a Point of Interest. Press 'enter' if you would like to leave this blank: ")
+        location = input("Please enter a Point of Interest. Press 'enter' if you would like to view all crimes by Points of Interest: ")
         crimesByLocation(location)
 
     elif keyWord == "4":
-        lsoa_name = input("Please enter a LSOA name. Press 'enter' if you would like to leave this blank: ")
+        lsoa_name = input("Please enter a LSOA name. Press 'enter' if you would like to view all crimes by LSOA name: ")
         crimesByLSOA(lsoa_name)
 
     elif keyWord == "5":
-        outcome = input("Please enter an outcome category. Press 'enter' if you would like to leave this blank: ")
+        outcome = input("Please enter an outcome category. Press 'enter' if you would like to view all crimes by outcome category: ")
         crimesByOutcome(outcome)
 
     elif keyWord == "6":
-        location = input("Please enter a location. Press 'enter' if you would like to leave this blank: ")
-        crime_type = input("Please enter crime type. Press 'enter' if you would like to leave this blank: ")
+        location = input("Please enter a location. Press 'enter' if you would like to view all locations: ")
+        crime_type = input("Please enter crime type. Press 'enter' if you would like to view all crime types: ")
         crimesByCrimeTypeLocation(crime_type, location)
 
     elif keyWord == "7":
-        crime_type = input("Please enter crime type. Press 'enter' if you would like to leave this blank: ")
-        outcome = input("Please enter an outcome category. Press 'enter' if you would like to leave this blank: ")
+        crime_type = input("Please enter crime type. Press 'enter' if you would like to view all crime types: ")
+        outcome = input("Please enter an outcome category. Press 'enter' if you would like to view all outcomes: ")
         crimesByTypeOutcome(crime_type, outcome)
 
     elif keyWord == "8":
-        location = input("Please enter a location. Press 'enter' if you would like to leave this blank: ")
-        outcome = input("Please enter an outcome category. Press 'enter' if you would like to leave this blank: ")
+        location = input("Please enter a location. Press 'enter' if you would like to view all locations: ")
+        outcome = input("Please enter an outcome category. Press 'enter' if you would like to view all outcomes: ")
         crimesByLocationOutcome(location, outcome)
 
     elif keyWord == "9":
-        outcome = input("Please enter an outcome category. Press 'enter' if you would like to leave this blank: ")
-        lsoa = input("Please enter a LSOA name. Press 'enter' if you would like to leave this blank: ")
+        outcome = input("Please enter an outcome category. Press 'enter' if you would like to view all outcomes: ")
+        lsoa = input("Please enter a LSOA name. Press 'enter' if you would like to view all LSOA names: ")
         crimesByLSOAOutcome(lsoa, outcome)
 
     elif keyWord == "10":
